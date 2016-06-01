093967e41cc762439f9fb056555fd815bf4471e2
Siddharth Jha <sid@sidjha.com>
2016-06-01 15:19:00 -0400

    Enabling numbers mode while editing list

--- sharedspace/EditListViewController.m
+++ sharedspace/EditListViewController.m
@@ -12,6 +12,9 @@
 
 @end
 
+int counter;
+NSString * mode;
+
 @implementation EditListViewController
 
 - (void)viewDidLoad {
@@ -45,51 +48,125 @@
 }
 
 - (void) switchModeToBullets {
+    mode = @"bullets";
     
 }
 
 - (void) switchModeToNumbers {
     
+    mode = @"numbers";
+    
 }
 
 - (void) switchModeToTodos {
+    
+    mode = @"todos";
     
 }
 
 - (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
     
+    // TODO: modify condition to check for flags for bullet/numbers/todos
+    
     if ([text isEqualToString:@"\n"]) {
         
-        
-        if (range.location == textView.text.length) {
-            // Simply add the newline and bullet point to the end
-            NSString *updatedText = [textView.text stringByAppendingString:@"\n\u2022 "];
-            [textView setText:updatedText];
+        if ([mode isEqualToString:@"bullets"]) {
+            
+            if (range.location == textView.text.length) {
+                // Simply add the newline and bullet point to the end
+                NSString *updatedText = [textView.text stringByAppendingString:@"\n\u2022 "];
+                [textView setText:updatedText];
+            }
+            
+            // Else if the replacement text is being added in the middle of
+            // the text view's text...
+            else {
+                
+                // Get the replacement range of the UITextView
+                UITextPosition *beginning = textView.beginningOfDocument;
+                UITextPosition *start = [textView positionFromPosition:beginning offset:range.location];
+                UITextPosition *end = [textView positionFromPosition:start offset:range.length];
+                UITextRange *textRange = [textView textRangeFromPosition:start toPosition:end];
+                
+                // Insert that newline character *and* a bullet point
+                // at the point at which the user inputted just the
+                // newline character
+                [textView replaceRange:textRange withText:@"\n\u2022 "];
+                
+                // Update the cursor position accordingly
+                NSRange cursor = NSMakeRange(range.location + @"\n\u2022 ".length, 0);
+                textView.selectedRange = cursor;
+                
+            }
+            // Then return "NO, don't change the characters in range" since
+            // you've just done the work already
+            return NO;
+            
+        } else if ([mode isEqualToString:@"numbers"]) {
+            if (range.location == textView.text.length) {
+                // Simply add the newline and bullet point to the end
+                NSString *updatedText = [textView.text stringByAppendingString:[NSString stringWithFormat:@"\n%d ", counter++]];
+                [textView setText:updatedText];
+            }
+            
+            // Else if the replacement text is being added in the middle of
+            // the text view's text...
+            else {
+                
+                // Get the replacement range of the UITextView
+                UITextPosition *beginning = textView.beginningOfDocument;
+                UITextPosition *start = [textView positionFromPosition:beginning offset:range.location];
+                UITextPosition *end = [textView positionFromPosition:start offset:range.length];
+                UITextRange *textRange = [textView textRangeFromPosition:start toPosition:end];
+                
+                // Insert that newline character *and* a bullet point
+                // at the point at which the user inputted just the
+                // newline character
+                [textView replaceRange:textRange withText:[NSString stringWithFormat:@"\n%d ", counter++]];
+                
+                NSString *str = [NSString stringWithFormat:@"\n%d ", counter];
+                // Update the cursor position accordingly
+                NSRange cursor = NSMakeRange(range.location + str.length, 0);
+                textView.selectedRange = cursor;
+                
+            }
+            // Then return "NO, don't change the characters in range" since
+            // you've just done the work already
+            return NO;
+
+        } else {
+            if (range.location == textView.text.length) {
+                // Simply add the newline and bullet point to the end
+                NSString *updatedText = [textView.text stringByAppendingString:@"\n\u2022 "];
+                [textView setText:updatedText];
+            }
+            
+            // Else if the replacement text is being added in the middle of
+            // the text view's text...
+            else {
+                
+                // Get the replacement range of the UITextView
+                UITextPosition *beginning = textView.beginningOfDocument;
+                UITextPosition *start = [textView positionFromPosition:beginning offset:range.location];
+                UITextPosition *end = [textView positionFromPosition:start offset:range.length];
+                UITextRange *textRange = [textView textRangeFromPosition:start toPosition:end];
+                
+                // Insert that newline character *and* a bullet point
+                // at the point at which the user inputted just the
+                // newline character
+                [textView replaceRange:textRange withText:@"\n\u2022 "];
+                
+                // Update the cursor position accordingly
+                NSRange cursor = NSMakeRange(range.location + @"\n\u2022 ".length, 0);
+                textView.selectedRange = cursor;
+                
+            }
+            // Then return "NO, don't change the characters in range" since
+            // you've just done the work already
+            return NO;
+
         }
     
-        // Else if the replacement text is being added in the middle of
-        // the text view's text...
-        else {
-            
-            // Get the replacement range of the UITextView
-            UITextPosition *beginning = textView.beginningOfDocument;
-            UITextPosition *start = [textView positionFromPosition:beginning offset:range.location];
-            UITextPosition *end = [textView positionFromPosition:start offset:range.length];
-            UITextRange *textRange = [textView textRangeFromPosition:start toPosition:end];
-            
-            // Insert that newline character *and* a bullet point
-            // at the point at which the user inputted just the
-            // newline character
-            [textView replaceRange:textRange withText:@"\n\u2022 "];
-            
-            // Update the cursor position accordingly
-            NSRange cursor = NSMakeRange(range.location + @"\n\u2022 ".length, 0);
-            textView.selectedRange = cursor;
-            
-        }
-        // Then return "NO, don't change the characters in range" since
-        // you've just done the work already
-        return NO;
     }
     
     // Else return yes
